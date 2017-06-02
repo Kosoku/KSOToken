654e21a0a17bcb419531b72f392d345c6c9598fc
William Towe <willbur1984@gmail.com>
2017-06-02 13:08:16 -0400

    Initial Commit

---
+++ KSOToken/KSOToken.h
@@ -0,0 +1,19 @@
+//
+//  KSOToken.h
+//  KSOToken
+//
+//  Created by William Towe on 6/2/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOToken.
+FOUNDATION_EXPORT double KSOTokenVersionNumber;
+
+//! Project version string for KSOToken.
+FOUNDATION_EXPORT const unsigned char KSOTokenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOToken/PublicHeader.h>
+
+
