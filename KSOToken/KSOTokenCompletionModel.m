5a091e884f46f1d53dba4c36e702b4405556fd29
William Towe <willbur1984@gmail.com>
2018-10-03 18:13:55 -0400

    allow NSString and NSURL to be used as completion model objects

--- KSOToken/KSOTokenCompletionModel.m
+++ KSOToken/KSOTokenCompletionModel.m
@@ -1,9 +1,9 @@
 //
-//  KSOTokenCompletionModel.h
+//  KSOTokenCompletionModel.m
 //  KSOToken
 //
-//  Created by William Towe on 6/5/17.
-//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//  Created by William Towe on 10/3/18.
+//  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //
@@ -13,40 +13,16 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <Foundation/Foundation.h>
+#import "KSOTokenCompletionModel.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
-/**
- Protocol for objects that will be displayed in the completions table view.
- */
-@protocol KSOTokenCompletionModel <NSObject>
-@required
-/**
- Return the title of the completion.
- */
-- (NSString *)tokenCompletionModelTitle;
-@optional
-/**
- Return the matching range of the completion.
- */
-- (NSRange)tokenCompletionModelRange;
-/**
- Return the matching ranges of the completion. If implemented, this is preferred over tokenCompletionModelRange.
- */
-- (NSIndexSet *)tokenCompletionModelIndexes;
+@implementation NSString (KSOTokenCompletionModelExtensions)
+- (NSString *)tokenCompletionModelTitle {
+    return self;
+}
 @end
 
-/**
- Add support for the KSOTokenCompletionModel protocol to NSString.
- */
-@interface NSString (KSOTokenCompletionModelExtensions) <KSOTokenCompletionModel>
+@implementation NSURL (KSOTokenCompletionModelExtensions)
+- (NSString *)tokenCompletionModelTitle {
+    return self.isFileURL ? self.lastPathComponent : self.absoluteString;
+}
 @end
-
-/**
- Add support for the KSOTokenCompletionModel protocol to NSURL.
- */
-@interface NSURL (KSOTokenCompletionModelExtensions) <KSOTokenCompletionModel>
-@end
-
-NS_ASSUME_NONNULL_END
